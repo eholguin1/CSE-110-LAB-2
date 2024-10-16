1161564ded29838148594268d0779c749bf69a3c
eholguin1 <eholguin@ucsd.edu>
2024-10-15 22:34:21 -0700

    Part 4

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,6 @@
 import './App.css';
+import { Label, Note } from "./types";
+import { dummyNotesList } from "./constants"; 
 
 function App() {
  return (
@@ -8,16 +10,21 @@ function App() {
       <div><textarea></textarea></div>
       <div><button type="submit">Create Note</button></div>
     </form>
-     <div className="note-individual">
-       <button>
-         x
-       </button>
-       <h2> 1st Note Title </h2>
-       <p> 1st Note Content </p>
-       <p> 1st Note Label </p>
+    <div className="notes-grid">
+       {dummyNotesList.map((note) => (
+         <div
+           key={note.id}
+           className="note-item">
+           <div className="notes-header">
+             <button>x</button>
+           </div>
+           <h2> {note.title} </h2>
+           <p> {note.content} </p>
+           <p> {note.label} </p>
+         </div>
+       ))}
      </div>
-   </div>
-
+    </div>
  );
 }
 
